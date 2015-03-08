cf928036e0a4338d8902312fbea73cca3d288670
Rick Bassham <brodrick.bassham@gmail.com>
2015-03-08 16:55:24 -0500

    delete on successful conversion

--- encode.py
+++ encode.py
@@ -63,200 +63,206 @@ def encode(movie):
     audio_i = 0
     subtitle_i = 0
 
-    print movie['name']
-
-    for stream in movie['streams']:
-        if stream['codec_type'] == 'video':
-            if video_stream is None:
-                video_stream = stream
-                video_stream_index = video_i
-            elif stream['width'] > video_stream['width'] and stream['width'] <= 1280:
-                video_stream = stream
-                video_stream_index = video_i
-            video_i = video_i + 1
-        elif stream['codec_type'] == 'audio':
-            if audio_stream is None:
-                audio_stream = stream
-                audio_stream_index = audio_i
-            elif stream['channels'] > audio_stream['channels'] or stream.get('bit_rate', 0) > audio_stream.get('bit_rate', 0):
-                audio_stream = stream
-                audio_stream_index = audio_i
-            audio_i = audio_i + 1
-
-    for stream in movie['streams']:
-        if stream['codec_type'] == 'subtitle':
-            if audio_stream is not None:
-                audio_stream_lang = audio_stream['tags'].get('language', audio_stream['tags'].get('LANGUAGE', 'eng'))
-
-                if audio_stream_lang == 'eng' and stream['disposition']['forced'] and stream['tags']['language'] == 'eng':
-                    subtitle_stream = stream
-                    subtitle_stream_index = subtitle_i
-                elif audio_stream_lang != 'eng' and stream['tags']['language'] == 'eng':
-                    subtitle_stream = stream
-                    subtitle_stream_index = subtitle_i
-            subtitle_i = subtitle_i + 1
-
-    if subtitle_stream_index < 0:
-        subtitle_stream_index = 0
-
     input_file = os.path.join(movie['folder'], movie['name'] + movie['extn'])
-    temp_file = movie['name'] + '.mp4'
-    subtitle_file = os.path.join(movie['folder'], movie['name'] + '.srt')
 
-    output_file = None
+    if os.path.isfile(input_file):
+        print movie['name']
 
-    if include_relative_path:
-        output_file = os.path.join(movie['folder'], movie['name'] + '.mp4').replace(search_path, converted_path)
-    else:
-        output_file = os.path.join(converted_path, movie['name'] + '.mp4')
+        for stream in movie['streams']:
+            if stream['codec_type'] == 'video':
+                if video_stream is None:
+                    video_stream = stream
+                    video_stream_index = video_i
+                elif stream['width'] > video_stream['width'] and stream['width'] <= 1280:
+                    video_stream = stream
+                    video_stream_index = video_i
+                video_i = video_i + 1
+            elif stream['codec_type'] == 'audio':
+                if audio_stream is None:
+                    audio_stream = stream
+                    audio_stream_index = audio_i
+                elif stream['channels'] > audio_stream['channels'] or stream.get('bit_rate', 0) > audio_stream.get('bit_rate', 0):
+                    audio_stream = stream
+                    audio_stream_index = audio_i
+                audio_i = audio_i + 1
 
-    mkdir_p(os.path.dirname(output_file))
+        for stream in movie['streams']:
+            if stream['codec_type'] == 'subtitle':
+                if audio_stream is not None:
+                    audio_stream_lang = audio_stream['tags'].get('language', audio_stream['tags'].get('LANGUAGE', 'eng'))
 
-    use_srt_file = subtitle_stream is None and os.path.isfile(subtitle_file)
-    srt_file_encoding = None
+                    if audio_stream_lang == 'eng' and stream['disposition']['forced'] and stream['tags']['language'] == 'eng':
+                        subtitle_stream = stream
+                        subtitle_stream_index = subtitle_i
+                    elif audio_stream_lang != 'eng' and stream['tags']['language'] == 'eng':
+                        subtitle_stream = stream
+                        subtitle_stream_index = subtitle_i
+                subtitle_i = subtitle_i + 1
 
-    if use_srt_file:
-        raw = open(subtitle_file).read()
-        srt_file_encoding = chardet.detect(raw)['encoding']
+        if subtitle_stream_index < 0:
+            subtitle_stream_index = 0
 
-    copy_video = False
-    copy_audio = False
+        temp_file = movie['name'] + '.mp4'
+        subtitle_file = os.path.join(movie['folder'], movie['name'] + '.srt')
 
-    command = [
-        avconv_path,
-        "-hide_banner",
-#        "-loglevel", "error",
-        "-y",
-        "-i", input_file,
-    ]
+        output_file = None
 
-    if srt_file_encoding is not None:
-        command.extend([
-            "-sub_charenc", srt_file_encoding,
-        ])
-
-    if use_srt_file:
-        command.extend([
-            "-i", subtitle_file,
-        ])
-
-    command.extend([
-        "-map", "0:v:{0}".format(video_stream_index),
-        "-map_metadata", "-1",
-        "-strict", "experimental",
-    ])
-
-    valid_profiles = ['High', 'Main', 'Baseline', 'Constrained Baseline']
-
-    if subtitle_stream is None and video_stream['codec_name'] == 'h264' and video_stream['width'] <= 1280 and video_stream['level'] <= 41 and video_stream['profile'] in valid_profiles:
-        command.extend([
-            "-codec:v:{0}".format(video_stream_index), "copy",
-        ])
-        copy_video = True
-    else:
-        command.extend([
-            "-codec:v:{0}".format(video_stream_index), "libx264",
-#            "-threads", "2",
-            "-crf", "22.0",
-            "-profile:v", "high",
-            "-filter:v:{0}".format(video_stream_index), "scale=w='min(1280\, iw):trunc(ow/a/2)*2'",
-            "-x264-params", "level=41:cabac=1:vbv-maxrate=10000:vbv-bufsize=20000",
-        ])
-
-    if audio_stream['channels'] == 2 and audio_stream['codec_name'] == 'aac':
-        command.extend([
-            "-map", "0:a:{0}".format(audio_stream_index),
-            "-codec:a:{0}".format(audio_stream_index), "copy",
-        ])
-        copy_audio = True
-    else:
-        command.extend([
-            "-map", "0:a:{0}".format(audio_stream_index),
-            "-codec:a:{0}".format(audio_stream_index), "aac",
-            "-b:a:{0}".format(audio_stream_index), "160k",
-        ])
-
-        if audio_stream['channels'] > 2:
-            command.extend([
-                "-ac", "2",
-                "-af", "aresample=matrix_encoding=dplii",
-            ])
+        if include_relative_path:
+            output_file = os.path.join(movie['folder'], movie['name'] + '.mp4').replace(search_path, converted_path)
         else:
+            output_file = os.path.join(converted_path, movie['name'] + '.mp4')
+
+        mkdir_p(os.path.dirname(output_file))
+
+        use_srt_file = subtitle_stream is None and os.path.isfile(subtitle_file)
+        srt_file_encoding = None
+
+        if use_srt_file:
+            raw = open(subtitle_file).read()
+            srt_file_encoding = chardet.detect(raw)['encoding']
+
+        copy_video = False
+        copy_audio = False
+
+        command = [
+            avconv_path,
+            "-hide_banner",
+    #        "-loglevel", "error",
+            "-y",
+            "-i", input_file,
+        ]
+
+        if srt_file_encoding is not None:
+            command.extend([
+                "-sub_charenc", srt_file_encoding,
+            ])
+
+        if use_srt_file:
+            command.extend([
+                "-i", subtitle_file,
+            ])
+
+        command.extend([
+            "-map", "0:v:{0}".format(video_stream_index),
+            "-map_metadata", "-1",
+            "-strict", "experimental",
+        ])
+
+        valid_profiles = ['High', 'Main', 'Baseline', 'Constrained Baseline']
+
+
+        if video_stream['width'] <= 1280:
+            copy_video = True
+
+        if audio_stream['channels'] <= 2:
             copy_audio = True
 
-    if subtitle_stream is not None:
+
+        if subtitle_stream is None and video_stream['codec_name'] == 'h264' and video_stream['width'] <= 1280 and video_stream['level'] <= 41 and video_stream['profile'] in valid_profiles:
+            command.extend([
+                "-codec:v:{0}".format(video_stream_index), "copy",
+            ])
+        else:
+            command.extend([
+                "-codec:v:{0}".format(video_stream_index), "libx264",
+    #            "-threads", "2",
+                "-crf", "22.0",
+                "-profile:v", "high",
+                "-filter:v:{0}".format(video_stream_index), "scale=w='min(1280\, iw):trunc(ow/a/2)*2'",
+                "-x264-params", "level=41:cabac=1:vbv-maxrate=10000:vbv-bufsize=20000",
+            ])
+
+        if audio_stream['channels'] == 2 and audio_stream['codec_name'] == 'aac':
+            command.extend([
+                "-map", "0:a:{0}".format(audio_stream_index),
+                "-codec:a:{0}".format(audio_stream_index), "copy",
+            ])
+        else:
+            command.extend([
+                "-map", "0:a:{0}".format(audio_stream_index),
+                "-codec:a:{0}".format(audio_stream_index), "aac",
+                "-b:a:{0}".format(audio_stream_index), "160k",
+            ])
+
+            if audio_stream['channels'] > 2:
+                command.extend([
+                    "-ac", "2",
+                    "-af", "aresample=matrix_encoding=dplii",
+                ])
+
+        if subtitle_stream is not None:
+            command.extend([
+                "-vf", 'subtitles=filename={0}:stream_index={1}'.format(input_file, subtitle_stream_index)
+            ])
+        elif use_srt_file:
+            command.extend([
+                "-map", "1:s:{0}".format(subtitle_stream_index),
+                "-c:s:{0}".format(subtitle_stream_index), "mov_text",
+                "-metadata:s:s:{0}".format(subtitle_stream_index), "language=eng",
+            ])
+
         command.extend([
-            "-vf", 'subtitles=filename={0}:stream_index={1}'.format(input_file, subtitle_stream_index)
-        ])
-    elif use_srt_file:
-        command.extend([
-            "-map", "1:s:{0}".format(subtitle_stream_index),
-            "-c:s:{0}".format(subtitle_stream_index), "mov_text",
-            "-metadata:s:s:{0}".format(subtitle_stream_index), "language=eng",
+            temp_file,
         ])
 
-    command.extend([
-        temp_file,
-    ])
+        if os.path.isfile(output_file):
+            return
 
-    if os.path.isfile(output_file):
-        return
+        print ' '.join(command)
 
-    print ' '.join(command)
+        f = open('commands.log', 'a')
+        f.write(movie['name'])
+        f.write('\n')
+        f.write(' '.join(command))
+        f.write('\n')
+        f.write('\n')
+        f.close()
 
-    f = open('commands.log', 'a')
-    f.write(movie['name'])
-    f.write('\n')
-    f.write(' '.join(command))
-    f.write('\n')
-    f.write('\n')
-    f.close()
-
-
-    ret = subprocess.call(command)
-
-    if ret == 0:
-        command = [
-            MP4Box_path,
-            "-isma",
-            "-inter", "500",
-            temp_file
-        ]
 
         ret = subprocess.call(command)
 
         if ret == 0:
-            print 'Copying to final destination...'
-            try:
-                shutil.move(temp_file, output_file + '.tmp')
-            except:
-                pass
+            command = [
+                MP4Box_path,
+                "-isma",
+                "-inter", "500",
+                temp_file
+            ]
 
-            try:
-                shutil.move(output_file + '.tmp', output_file)
-            except:
-                pass
+            ret = subprocess.call(command)
 
-            try:
-                os.remove(temp_file)
-            except:
-                pass
+            if ret == 0:
+                print 'Copying to final destination...'
+                try:
+                    shutil.move(temp_file, output_file + '.tmp')
+                except:
+                    pass
 
-            if copy_video and copy_audio and os.path.isfile(output_file):
-                os.remove(input_file)
+                try:
+                    shutil.move(output_file + '.tmp', output_file)
+                except:
+                    pass
 
+                try:
+                    os.remove(temp_file)
+                except:
+                    pass
+
+                if copy_video and copy_audio and os.path.isfile(output_file):
+                    os.remove(input_file)
+
+            else:
+                f = open('errors.log', 'a')
+                f.write(movie['name'])
+                f.write('\n')
+                f.write(' '.join(command))
+                f.write('\n')
         else:
             f = open('errors.log', 'a')
             f.write(movie['name'])
             f.write('\n')
             f.write(' '.join(command))
             f.write('\n')
-    else:
-        f = open('errors.log', 'a')
-        f.write(movie['name'])
-        f.write('\n')
-        f.write(' '.join(command))
-        f.write('\n')
 
 
 for video in to_process['videos']:
